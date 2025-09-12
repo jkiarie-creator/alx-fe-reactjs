5f6dd496af1c4bfd3584872042bc75e6c2202f46
jkiarie-creator <josmwangi101@gmail.com>
2025-09-12 13:38:41 +0300

    Add recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  )
+}
+
+export default App
