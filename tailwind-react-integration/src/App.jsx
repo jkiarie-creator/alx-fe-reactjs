aadc17e449afe51881ecd0829f957bceb85cc18d
jkiarie-creator <josmwangi101@gmail.com>
2025-10-01 10:43:08 +0300

    Add UserProfile component

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
