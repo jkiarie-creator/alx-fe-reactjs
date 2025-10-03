6671aa8442c167733c40e9ebf88284b557e34287
jkiarie-creator <josmwangi101@gmail.com>
2025-10-03 13:16:36 +0300

    Adjust tailwind config

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./public/index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}","./public/index.html","./index.html"],
   darkMode: false,
   theme: {
     extend: {},
