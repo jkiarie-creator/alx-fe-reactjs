5e716b85cb9fe50f7a584b0e0e083e10b042b2d8
jkiarie-creator <josmwangi101@gmail.com>
2025-10-17 11:45:57 +0300

    Adjust Yup validation schema

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -3,9 +3,9 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username: Yup.string().trim().required("Username is required"),
-  email: Yup.string().trim().email("Invalid email address").required("Email is required"),
-  password: Yup.string().trim().required("Password is required"),
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email address").required("Email is required"),
+  password: Yup.string().required("Password is required"),
 });
 
 export default function FormikForm() {
